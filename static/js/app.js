8c12240dc5783b77ca9e9d9fa0a257505284c91e
Joseph Lubrano <jlubranos@optimum.net>
2020-12-10 10:34:39 -0500

    12-10-2020

--- static/js/app.js
+++ static/js/app.js
@@ -17,11 +17,49 @@ function convertDate(searchDate) {
 return searchDate;
 }
 
+function clearTable() {
+    let numberRows=document.getElementById("ufo-table").rows.length;
+    if (numberRows>2) {
+        for (let i=0; i<numberRows-2; i++) {
+            document.getElementById("ufo-table").deleteRow(2);
+        }
+    }
+}
+
+function insertTablerow(tableData) {
+    console.log(tableData.datetime);
+    console.log(tableData.city);
+    console.log(tableData.state);
+    console.log(tableData.country);
+    console.log(tableData.shape);
+    console.log(tableData.durationMinutes);
+    console.log(tableData.comments);
+
+    let numberRows=document.getElementById("ufo-table").rows.length;
+    let newRow=document.getElementById('ufo-table').insertRow(numberRows);
+
+    let datetime=newRow.insertCell(0);
+    let city=newRow.insertCell(1);
+    let state=newRow.insertCell(2);
+    let country=newRow.insertCell(3);
+    let shape=newRow.insertCell(4);
+    let durationMinutes=newRow.insertCell(5);
+    let comments=newRow.insertCell(6);
+
+    datetime.innerHTML=tableData.datetime;
+    city.innerHTML=tableData.city;
+    state.innerHTML=tableData.state;
+    country.innerHTML=tableData.country;
+    shape.innerHTML=tableData.shape;
+    durationMinutes.innerHTML=tableData.durationMinutes;
+    comments.innerHTML=tableData.comments;
+}
+
 function readList(searchDate) {
     let found=false;
     for(let i=0; i<tableData.length; i++) {
         if (searchDate==tableData[i].datetime) {
-            console.log(tableData[i].datetime);
+            insertTablerow(tableData[i]);
             found=true;
         }
     }
@@ -32,13 +70,14 @@ function readList(searchDate) {
 
 function checksited(event) {
     let x=event.which || event.keyCode;
-    if (x==13) {
-        console.log("Enter Key Pressed...");
-        let searchDate=document.getElementById("datetime").value;
-        console.log("Search Date:",searchDate);
-        if (isDate(searchDate)) {
-            searchDate=convertDate(searchDate);
-            readList(searchDate);
+ //   if (x==13) {
+ //       console.log("Enter Key Pressed...");
+    let searchDate=document.getElementById("datetime").value;
+//        console.log("Search Date:",searchDate);
+    if (isDate(searchDate)) {
+        searchDate=convertDate(searchDate);
+        clearTable();
+        readList(searchDate);
         }
-    }
+ //   }
 }
